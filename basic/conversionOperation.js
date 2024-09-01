b8bbf364f20feed55f71f08ec442b0baaa4f6b44
Sonali Parihar <sonalipariharsrist@gmail.com>
2024-09-01 10:46:16 +0000

    datatype conversion

---
+++ basic/conversionOperation.js
@@ -0,0 +1,23 @@
+// let score = "sonali";
+// console.log(typeof score) ;
+// console.log(typeof(score));
+// let valueInNum = Number(score);
+// console.log(typeof valueInNum ) ;
+
+// console.log(valueInNum)
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+//  let isLoggedIn ="sonali";
+//  let booleanNum = Boolean(isLoggedIn)
+//  console.log(booleanNum);
+ // 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber );
+console.log( stringNumber);
+
+
+
