8cd99c1060e8f16e9ca42e2bf9d7ab22fc1b87a8
Sonali Parihar <sonalipariharsrist@gmail.com>
2024-09-10 08:55:39 +0000

    for avoid hoisting in js

--- basic/Hoisting.js
+++ basic/Hoisting.js
@@ -16,3 +16,15 @@
 //  }
 //  hoistedFunction();
  //output 3 even when the variable is declared after it is initialized	in the function
+ 
+//  var x;
+// console.log(x); 
+// x = 23;
+// Outputs "undefined" since the initialization of "x" is not hoisted
+
+//Note - To avoid hoisting, you can run javascript in strict mode by using “use strict” on top of the code:
+
+"use strict";
+x = 23; 
+var x;
+
