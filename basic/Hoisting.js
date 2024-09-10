68a8800ce3004a8e8ebe6109b4925cd1be438e2e
Sonali Parihar <sonalipariharsrist@gmail.com>
2024-09-10 08:02:52 +0000

    hoisting in javascript

---
+++ basic/Hoisting.js
@@ -0,0 +1,18 @@
+// hoistedVariable=3;
+// console.log(hoistedVariable)
+// var hoistedVariable;
+//output 3 even when the variable is declared after it is initialized	
+
+// hoistedFunction();  
+// function hoistedFunction(){ 
+//   console.log(" Hello world! ");
+// } 
+// Outputs " Hello world! " even when the function is declared after calling
+
+//  function hoistedFunction(){
+//     x=3;
+//     console.log(x);
+//     var x;
+//  }
+//  hoistedFunction();
+ //output 3 even when the variable is declared after it is initialized	in the function
