db9f0da622b251639468c597c8e1f2c7ba86338f
Sonali Parihar <sonalipariharsrist@gmail.com>
2024-09-22 12:57:27 +0000

    number basic

---
+++ basic/numMath.js
@@ -0,0 +1,10 @@
+const score=400;
+console.log(score)
+const balance= new Number(100)
+console.log(balance.toString().length)
+const otherNumber=123.8997
+console.log(otherNumber.toFixed(2))
+console.log(otherNumber.toPrecision(4))
+
+
+
