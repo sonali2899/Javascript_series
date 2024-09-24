1dc15bdf9866cdca956796a6fa11d6030d0857a9
Sonali Parihar <sonalipariharsrist@gmail.com>
2024-09-24 09:03:55 +0000

    number and math basic

--- basic/numMath.js
+++ basic/numMath.js
@@ -2,9 +2,32 @@ const score=400;
 console.log(score)
 const balance= new Number(100)
 console.log(balance.toString().length)
-const otherNumber=123.8997
+const otherNumber=1123.8997
 console.log(otherNumber.toFixed(2))
-console.log(otherNumber.toPrecision(4))
+console.log(otherNumber.toPrecision(5))
+const num=1000000
+console.log(num.toLocaleString('en-IN'))
+console.log(Number.MIN_VALUE)//min,max
+
+//++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++
+
+console.log(Math);
+const a=-4
+const b=123.4
+console.log(Math.abs(a));
+console.log(Math.round(b));
+console.log(Math.ceil(b));
+console.log(Math.floor(b));
+console.log(Math.min(5,4,8,6,9))
+console.log(Math.max(5,4,8,6,9))
+
+console.log((Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
 
 
 
