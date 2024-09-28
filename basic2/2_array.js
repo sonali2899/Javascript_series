a7d702fffcd3e57738b8c0cc894526827253f648
Sonali Parihar <sonalipariharsrist@gmail.com>
2024-09-28 06:23:44 +0000

    array basic

---
+++ basic2/2_array.js
@@ -0,0 +1,27 @@
+const car=['volvo','suv','bmw']
+const heros=['batman','spiderman','superman']
+// car.push(heros)
+// console.log(car);
+// const newarr=car.concat(heros)
+// console.log(newarr);
+// const newarr2=[...car,...heros]
+// console.log(newarr2);
+// const myarr=[1,3,4,[1,2,3,[2,2,3,4]],9,7]
+// const real=myarr.flat(Infinity)
+// console.log(real);
+console.log(Array.isArray('sonali'));
+console.log(Array.from('sonali'));
+console.log(Array.from({name:'sonali'})) //if we log object we will get empty erray
+const score1=100
+const score2=200
+const score3=300
+const score4=400
+console.log(Array.of(score1,score2,score3,score4))
+
+
+
+
+
+
+
+
---
+++ basic2/array.js
@@ -0,0 +1,37 @@
+//how to declare array in JS
+const myarr=[1,2,3,4,5,6,7]
+const myarr1=["shaktiman","nagraj","hatim"]
+const myarr2=new Array(1,2,3,4)
+const myarr3=[1,2,3,"sonali"]
+ 
+//array methods
+// myarr.push(8)
+// console.log(myarr);
+// myarr.pop()
+// console.log(myarr);
+// myarr.unshift(0)
+// console.log(myarr);
+// myarr.shift()
+// console.log(myarr);
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(3));
+// const newarr=myarr.join()
+// console.log(myarr);
+// console.log(typeof newarr);
+// console.log("A",myarr);
+// const myn1=myarr.slice(1,4)
+// console.log("B",myn1);
+// console.log("c",myarr);
+// const myn2=myarr.splice(1,4)
+// console.log("d",myn2);
+// console.log("e",myarr);
+
+
+
+
+
+
+
+
+
+
