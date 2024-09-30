b300a6f1bf9817544b0f27111223c1bc9fe13ecb
Sonali Parihar <sonalipariharsrist@gmail.com>
2024-09-30 06:20:15 +0000

    function basic

---
+++ basic3/function1.js
@@ -0,0 +1,14 @@
+function sayMyName(){
+    console.log("hello sonali")
+}
+sayMyName()
+function add(num1,num2){
+   console.log (num1+num2)
+}
+add(1,2)
+function sub(num1,num2){
+   let result=(num1-num2) 
+   return result
+ }
+const result=sub(10,3)
+console.log(result);
