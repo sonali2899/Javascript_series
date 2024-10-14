3daeeaa1b502810dcde7132eb2ea9cac6573444b
Sonali Parihar <sonalipariharsrist@gmail.com>
2024-10-14 07:49:27 +0000

    basics

--- basic3/function1.js
+++ basic3/function1.js
@@ -12,3 +12,44 @@ function sub(num1,num2){
  }
 const result=sub(10,3)
 console.log(result);
+
+function loggedInUser(username='sam'){
+   if(!username){
+      console.log("please enter username");
+      return
+   }
+   return `${username} just logged in`;
+  }
+//  const message= loggedInUser('sonali')
+//  console.log(message)
+ console.log(loggedInUser('sonali'))
+ console.log(loggedInUser());
+  
+ function calculateCartPrice(...num1){
+   return num1
+ }
+console.log( calculateCartPrice(100,200,500,600));
+
+const user={
+   username:"sonali",
+   price :700
+}
+function handleObject(anyObject){
+console.log(`username is ${anyObject.username},price is ${anyObject.price}`);
+
+}
+// handleObject(user)
+handleObject({
+   username:"sonali",
+   price :700
+})
+const newArray=[200,400,500,100]
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+// console.log(returnSecondValue(newArray));
+console.log(returnSecondValue([200,400,500,100]))
+
+
+
+
---
+++ basic3/scopes.js
@@ -0,0 +1,26 @@
+// var c=300
+let a=300
+if(true){
+    let a=7
+    const b=10
+    // var c=30
+    console.log('inner ',a);
+    
+}
+// console.log(a);
+// console.log(b);
+console.log(a);
+ 
+function one(){
+  const username="sonali"  
+  function two() {
+    const website = "youtube"
+    console.log(username);
+    }
+  two()
+//console.log(website);
+  
+}
+one()
+
+
