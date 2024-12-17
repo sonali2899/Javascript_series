2658ba45fee3f758272ac4bc4de986dc40a69732
Sonali Parihar <sonalipariharsrist@gmail.com>
2024-12-17 01:58:26 +0000

    control flow basic

---
+++ 4_control_flow/switch.js
@@ -0,0 +1,46 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// const month="march"
+// switch (month) {
+//     case "jan":
+//     console.log("january");
+//     break;
+//     case "feb":
+//     console.log("febuary");
+//     break;
+//     case "march":
+//     console.log("march");
+//     break;
+//     case "april":
+//     console.log("april");
+//     break;
+
+//     default:
+//     console.log("default value");
+//     break;
+// }
+ const menu = 3
+ switch(menu){
+    case 1 :
+    console.log("samosa");
+     break;
+    case 2 :
+    console.log("kachori");
+    break;
+    case 3 :
+    console.log("chai");
+    break;
+    case 4 :
+    console.log("poha");
+    break;
+
+    default:
+    console.log('number is not on menu card');
+        
+}
